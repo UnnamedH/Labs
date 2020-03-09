11d64ee73738563946020f5ad39fa8703aee7805
zloy.goblin.ork@gmail.com <zloy.goblin.ork@gmail.com>
2020-03-09 13:48:30 +0200

    Добавьте файлы проекта.

---
+++ Labs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+
+        static void DifferenceTwoNums(ref double num1, ref double num2)
+        {
+            Console.WriteLine("Разность двух чисел: " + (num1 - num2));
+        }
+
+        static void Main(string[] args)
+        {
+            double num1 = 4, num2 = 3;
+            DifferenceTwoNums(ref num1, ref num2);
+
+
+        }
+    }
+}
